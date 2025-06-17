042ea322d882dbeb0ef8b7b3733e367994823d02
DilshanGAM <it21180934@my.sliit.lk>
2025-06-17 20:53:38 +0530

    Day 10 ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/registerPage";
+import AdminPage from "./pages/adminPage";
 
 function App() {
 	return (
-		<div className="w-full h-screen bg-blue-300">
-			<div className="w-[600px] h-[600px] bg-black flex flex-col justify-center ">
-				<div className=" w-[75px] h-[75px] bg-yellow-600"></div>
-				<div className="w-[75px] h-[75px] bg-red-900"></div>
-				<div className="w-[75px] h-[75px] bg-green-600"></div>
-				<div className="w-[75px] h-[75px] bg-purple-600"></div>
-				<div className="w-[75px] h-[75px] bg-pink-600"></div>
-				<div className="w-[75px] h-[75px] bg-orange-600"></div>
+		<BrowserRouter>
+			<div className="w-full h-screen flex justify-center items-center">
+				
+					<Routes path="/">
+						<Route path="/" element={<HomePage/>}/>
+						<Route path="/login" element={<LoginPage/>}/>
+						<Route path="/register" element={<RegisterPage/>}/>
+						<Route path="/admin/*" element={<AdminPage/>}/>
+					</Routes>
+				
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 }
 
