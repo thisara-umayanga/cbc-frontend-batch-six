1b4eb30351801d51ac0d25b570b8a7c03da6d99d
DilshanGAM <it21180934@my.sliit.lk>
2025-06-24 20:52:05 +0530

    Day 11 ✏️

---
+++ src/pages/testPage.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+export default function TestPage(){
+    //let count = 0;
+    const [count , setCount] = useState(0)
+    //const [name, setName] = useState("Malith")
+
+
+    function increment(){
+        setCount(6);
+    }
+
+    function decrement(){
+        setCount(7);
+    }
+    
+    setCount(5)
+
+    return(
+        <div className="w-full h-screen bg-amber-200 flex justify-center items-center">
+            <div className="w-[400px] h-[400px] bg-white flex flex-col justify-center items-center">
+
+                <h1 className="text-5xl font-bold">{count}</h1>
+
+                <div className="w-full flex justify-center items-center  h-[100px]">
+                    <button onClick={decrement} className="w-[100px] bg-blue-500 h-[45px] text-3xl mx-2 flex justify-center items-center text-white rounded-full">
+                        -
+                    </button>
+
+                    <button onClick={increment} className="w-[100px] bg-blue-500 h-[45px] text-3xl mx-2 flex justify-center items-center text-white rounded-full">
+                        +
+                    </button>
+                </div>
+            </div>
+        </div>
+    ) 
+}
